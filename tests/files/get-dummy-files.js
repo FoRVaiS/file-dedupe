099e313f5a4ed15853d40054e46ba6b389aff066
Forvana <chiangbenjamin991@gmail.com>
2018-06-10 15:39:37 -0700

    Centralize dummy test files

---
+++ tests/files/get-dummy-files.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = [
+    path.join('tests', 'files', 'dummy-1.dummy'),
+    path.join('tests', 'files', 'dummy-2.dummy'),
+    path.join('tests', 'files', 'dummy-3.dummy'),
+    path.join('tests', 'files', 'dummy-4.dummy'),
+];
