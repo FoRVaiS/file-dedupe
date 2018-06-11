5f542066faee27e9adadeeed10ad3bcc1265ff84
Forvana <chiangbenjamin991@gmail.com>
2018-06-11 08:03:43 -0700

    Create tests for delete-duplicates

---
+++ tests/delete-duplicates/delete-duplicates.test.js
@@ -0,0 +1,28 @@
+const deleteDuplicatesFactory = require('../../lib/delete-duplicates/delete-duplicates');
+const getDummyFiles = require('../files/get-dummy-files');
+
+describe('Delete Duplicates', () => {
+    it('should delete correct file paths', () => {
+        const fakeDependencies = {
+            fs: {
+                unlink: jest.fn(),
+            },
+        };
+
+        const fakeDuplicates = {
+            duplicates: [
+                getDummyFiles[0],
+                getDummyFiles[1],
+                getDummyFiles[2],
+            ],
+        };
+
+        const deleteDuplicates = deleteDuplicatesFactory(fakeDependencies);
+
+        deleteDuplicates(fakeDuplicates);
+
+        expect(fakeDependencies.fs.unlink.mock.calls[0][0]).toBe(getDummyFiles[0]);
+        expect(fakeDependencies.fs.unlink.mock.calls[1][0]).toBe(getDummyFiles[1]);
+        expect(fakeDependencies.fs.unlink.mock.calls[2][0]).toBe(getDummyFiles[2]);
+    });
+});
