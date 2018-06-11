ac32421da37b51907834478859c3dfa7da86d891
Forvana <chiangbenjamin991@gmail.com>
2018-06-11 08:49:06 -0700

    Remove feature folders

--- tests/group-file-size.test.js
+++ tests/group-file-size.test.js
@@ -1,5 +1,5 @@
-const groupFileSize = require('../../lib/group-file-size/group-file-size');
-const getDummyFiles = require('../files/get-dummy-files');
+const groupFileSize = require('../lib/group-file-size');
+const getDummyFiles = require('./files/get-dummy-files');
 
 describe('Group File Size', () => {
     it('should return an array of file pathes grouped by file sizes', () => {
