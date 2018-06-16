e8e05f0715f67a0f2df3b50e4972d761a7f00d2b
Forvana <chiangbenjamin991@gmail.com>
2018-06-16 08:02:41 -0700

    Create extra dummy file for sort testing

--- tests/files/get-dummy-files.js
+++ tests/files/get-dummy-files.js
@@ -6,4 +6,5 @@ module.exports = [
     path.join('tests', 'files', 'dummy-3.dummy'),
     path.join('tests', 'files', 'dummy-4.dummy'),
     path.join('tests', 'files', 'dummy-5.dummy'),
+    path.join('tests', 'files', 'dummy-6.dummy'),
 ];
