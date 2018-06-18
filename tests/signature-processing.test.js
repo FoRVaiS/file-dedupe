ed6dbb44c9ee7f70680ce1cd66e0402bcd4897ad
Forvana <chiangbenjamin991@gmail.com>
2018-06-18 12:09:11 -0700

    Modify function to accept a source directory

--- tests/signature-processing.test.js
+++ tests/signature-processing.test.js
@@ -1,49 +1,117 @@
 const signatureProcessing = require('../lib/signature-processing');
 
 describe('Signature Processing', () => {
+    const fakeTarget = 'path/to/folder';
+    const fakeExclusions = ['ignore/directory1', 'ignore/file2.txt'];
+    const fakeOptions = { option1: false };
+
+    let fakeRun;
+
+    beforeEach(() => {
+        fakeRun = jest.fn();
+    });
+
     it('should demand a directory string', () => {
         expect(signatureProcessing()).toThrowError('You must define a target directory!');
     });
 
     it('should return the run function', () => {
-        const fakeRun = jest.fn().mockReturnValue(1);
+        fakeRun = jest.fn().mockReturnValue(1);
 
         expect(signatureProcessing(fakeRun)('filler')).toBe(1);
     });
 
-    describe('Single Target Directory', () => {
-        describe('Exclusions', () => {
-            const fakeTarget = 'path/to/file';
-            const fakeExclusions = ['ignore/directory1', 'ignore/file2.txt'];
-            const fakeOptions = { option1: false };
+    describe('Target Directory', () => {
+        it('should run checks against target', () => {
+            signatureProcessing(fakeRun)(fakeTarget);
 
-            let fakeRun;
+            expect(fakeRun.mock.calls[0]).toEqual([
+                '',
+                fakeTarget,
+                [],
+                {},
+            ]);
+        });
 
-            beforeEach(() => {
-                fakeRun = jest.fn();
-            });
+        it('should be able to tell if no exclusions were passed in', () => {
+            signatureProcessing(fakeRun)(fakeTarget, fakeOptions);
 
-            it('should run checks against target', () => {
-                signatureProcessing(fakeRun)(fakeTarget);
+            expect(fakeRun.mock.calls[0]).toEqual([
+                '',
+                fakeTarget,
+                [],
+                fakeOptions,
+            ]);
+        });
 
-                expect(fakeRun.mock.calls[0][0]).toEqual(fakeTarget);
-            });
+        it('should exclude specified paths', () => {
+            signatureProcessing(fakeRun)(fakeTarget, fakeExclusions);
 
-            it('should be able to tell if no exclusions were passed in', () => {
-                signatureProcessing(fakeRun)(fakeTarget, fakeOptions);
+            expect(fakeRun.mock.calls[0]).toEqual([
+                '',
+                fakeTarget,
+                fakeExclusions,
+                {},
+            ]);
+        });
 
-                expect(fakeRun.mock.calls[0]).toEqual([
-                    fakeTarget,
-                    [],
-                    fakeOptions,
-                ]);
-            });
+        it('should exclude specified paths with options', () => {
+            signatureProcessing(fakeRun)(fakeTarget, fakeExclusions, fakeOptions);
 
-            it('should exclude specified paths', () => {
-                signatureProcessing(fakeRun)(fakeTarget, fakeExclusions);
+            expect(fakeRun.mock.calls[0]).toEqual([
+                '',
+                fakeTarget,
+                fakeExclusions,
+                fakeOptions,
+            ]);
+        });
+    });
 
-                expect(fakeRun.mock.calls[0][1]).toEqual(fakeExclusions);
-            });
+    describe('Source & Target Directory', () => {
+        const fakeSource = 'path/to/src/folder';
+
+        it('should run checks against target while comparing to source', () => {
+            signatureProcessing(fakeRun)(fakeSource, fakeTarget);
+
+            expect(fakeRun.mock.calls[0]).toEqual([
+                fakeSource,
+                fakeTarget,
+                [],
+                {},
+            ]);
+        });
+
+        it('should be able to tell if no exclusions were passed in', () => {
+            signatureProcessing(fakeRun)(fakeSource, fakeTarget, fakeOptions);
+
+            expect(fakeRun.mock.calls[0]).toEqual([
+                fakeSource,
+                fakeTarget,
+                [],
+                fakeOptions,
+            ]);
+        });
+
+        it('should exclude specified paths', () => {
+            signatureProcessing(fakeRun)(fakeSource, fakeTarget, fakeExclusions);
+
+            expect(fakeRun.mock.calls[0]).toEqual([
+                fakeSource,
+                fakeTarget,
+                fakeExclusions,
+                {},
+            ]);
+        });
+
+        it('should exclude specified paths with options', () => {
+            signatureProcessing(fakeRun)(fakeSource, fakeTarget, fakeExclusions, fakeOptions);
+
+            expect(fakeRun.mock.calls[0]).toEqual([
+                fakeSource,
+                fakeTarget,
+                fakeExclusions,
+                fakeOptions,
+            ]);
         });
     });
 });
